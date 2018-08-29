026a47c2329509ae7082952ac497e1db0b282645
maxim-maximov <Maxim.Maximov@returnonintelligence.com>
2018-08-30 01:29:37 +0300

    Start cell generator

--- src/app/Model/Game.ts
+++ src/app/Model/Game.ts
@@ -6,7 +6,7 @@ import { Ship } from "./Ship";
 export class Game {
 	readonly gameBoard: IGameCell[][]
 
-	constructor() {	
+	constructor() {
 
 		this.gameBoard = Array(10).fill(0).map(x => Array(10).fill(1).map(x => new EmptyCell()));
 
@@ -39,5 +39,30 @@ export class Game {
 		let ship = new Ship(1);
 
 		this.gameBoard[7][8] = ship.cells[0];
+	}
+
+	private insertToRandomPlace(cells: ShipCell[], offset: Array<[number, number]>) {
+		while (true) {
+			let startCoordinates = this.getRandomCellCoordinations();
+			
+			
+		 }
+
+	}
+
+	private canCellBePlacedHere(cell: ShipCell, coordinate:[number, number]) : boolean{
+
+
+		return false;
+	}
+
+
+	private getRandomCellCoordinations(): [number, number] {
+		return [this.randomInt(), this.randomInt()];
+	}
+	private randomInt(): number {
+		let min = 0;
+		let max = 9;
+		return Math.floor(Math.random() * (max - min + 1)) + min;
 	}
 }
