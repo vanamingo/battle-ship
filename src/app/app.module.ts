8c52135b10998b8a7e5f86ea03cebb99c2fad561
maxim-maximov <Maxim.Maximov@returnonintelligence.com>
2018-09-07 23:34:42 +0300

    Winner section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { BoardComponent } from './gamecomponents/board/board.component';
 import { GameCellComponent } from './gamecomponents/game-cell/game-cell.component';
 import { PlayerCardComponent } from './gamecomponents/player-card/player-card.component';
+import { WinnerSectionComponent } from './gamecomponents/winner-section/winner-section.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BoardComponent,
     GameCellComponent,
-    PlayerCardComponent
+    PlayerCardComponent,
+    WinnerSectionComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/gamecomponents/winner-section/winner-section.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Game } from '../../Model/Game';
+
+@Component({
+  selector: 'app-winner-section',
+  templateUrl: './winner-section.component.html',
+  styleUrls: ['./winner-section.component.css']
+})
+export class WinnerSectionComponent implements OnInit {
+  @Input() game: Game
+
+  constructor() { }
+
+  isGameFinished(): boolean{
+    return this.game.isGameFinished;
+  }
+
+  whoIsTheWinner(){
+    let stat = this.game.statistics;
+    if(stat.RobotShootHit > stat.UserShootHit){
+      return "robot";
+    }
+    
+    if(stat.RobotShootHit < stat.UserShootHit){
+      return "user";
+    }
+
+    return "friendship";
+  }
+
+  ngOnInit() {
+  }
+
+}
